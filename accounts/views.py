46bdf78661b6c17784b58a9f9cdd714059cd8dda
Gianni De vadder <giannidevadder0@gmail.com>
2023-05-12 12:32:30 +0200

    Push for correction assist

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
