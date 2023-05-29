3517517e6f81fe3aff5f5b6f9708ee3acfa5af90
Gianni De vadder <giannidevadder0@gmail.com>
2023-05-29 09:51:36 +0200

    Added Readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # openclassroom-projet-9
+Créez votre environnement virtuel avec "python -m venv .nomduvenv"
+
+Activ votre environnement virtuel avec ".nomduvenv/Script/activate.ps1 (Windows)"
+
+Clonez ce dépôt sur votre ordinateur avec "git clone https://github.com/Gianni-De-vadder/openclassroom-projet-9.git
+
+Accédez à la racine du répertoire cloné depuis un terminal
+
+Installez les dépendances avec  "pip install -r requirements.txt"
+
+Exécutez l'application avec python manage.py runserver
--- pages/views.py
+++ pages/views.py
@@ -5,8 +5,6 @@ from django.db.models import Q, Value, CharField
 from itertools import chain
 
 
-from django.db.models import Q
-
 @login_required
 def home_view(request):
     following_users = UserFollows.objects.filter(user=request.user).values("followed_user")
@@ -15,7 +13,7 @@ def home_view(request):
     # Modifier la requête des tickets pour inclure également les tickets de l'utilisateur
     tickets = Ticket.objects.filter(
         Q(user__in=following_users) | Q(user=user)
-    ).order_by("-time_created")
+    ).annotate(content_type=Value('TICKET', CharField())).order_by("-time_created")
 
     reviews = Review.objects.filter(
         Q(user__in=following_users) | Q(ticket__user=user)
@@ -25,10 +23,7 @@ def home_view(request):
         chain(reviews, tickets), key=lambda post: post.time_created, reverse=True
     )
 
-    tickets.annotate(content_type=Value('TICKET', CharField()))
-
     context = {
-        "tickets": tickets,
-        "reviews": reviews,
+        "posts": posts,
     }
     return render(request, "pages/home.html", context)
